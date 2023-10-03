e16107b3f921c9267dcbc10f269b323abd1478e8
Ericcyyyy <eric.michael.yo@gmail.com>
2023-10-02 18:48:18 -0700

    - Implement the navigation library (import gradle) - Create a navigation graph (nav_graph.xml) - Link the crimeListFragment.kt and crimeDetailFragment.kt in nav graph - Implement NavController to navigate - Implement safears for navigation in gradle - Implement id retrieval to get the details between clicking crime from ListFragment to crimeDetailFragment - Unidirectional data flow to allow users to modify details - Create a CrimeDetailViewModel.kt and CrimeDetailViewModelFactory class - Implement changes so crimeDetailFragment displays the proper details - Implement save in changes for crimeDetailFragment - Implement @update function in CrimeDao.kt - Expose function in CrimeRepository.kt - Implement global scope for coroutinescope in crimerepository to not lose the edited data in crimedetailfragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -54,6 +55,8 @@ dependencies {
     implementation("androidx.room:room-runtime:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     ksp("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
